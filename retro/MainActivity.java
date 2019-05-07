6995b2ba5edf456efc3c93f89ec9e6243d84b2d3
maliksenpai <37189443+maliksenpai@users.noreply.github.com>
2019-05-07 22:08:53 +0300

    Update MainActivity.java

--- retro/MainActivity.java
+++ retro/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.retro;
 
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,15 +19,27 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    public RecyclerView recyclerView;
+    public ArrayList<Model> arrayList=new ArrayList<>();
+    public int b=0;
+    public int i=0;
+    public String limit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final RecyclerView recyclerView=findViewById(R.id.liste);
-        final ArrayList<Model> arrayList=new ArrayList<>();
         textView=findViewById(R.id.isim);
+        api();
+        final int pastVisiblesItems, visibleItemCount, totalItemCount;
+        final RecyclerView recyclerView=findViewById(R.id.liste);
+    }
+    public void api(){
+        final RecyclerView recyclerView=findViewById(R.id.liste);
+        i=b;
+        b=b+20;
+        limit=String.valueOf(b);
         ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        Call<Gelenler> call= apiInterface.getMovieDetails();
+        final Call<Gelenler> call= apiInterface.getMovieDetails(limit);
         Log.d("gelenler","birşey oldu");
         call.enqueue(new Callback<Gelenler>() {
             @Override
@@ -33,15 +48,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("gelenler", String.valueOf(response.body()));
                 Log.d("gelenler", String.valueOf(response.body().getTracks().getTrack().get(1).getName())); //null
                 Log.d("gelenler", String.valueOf(response.body().getTracks().getTrack().size()));
-                int i=0;
-                for(i=0;i<response.body().getTracks().getTrack().size();i++){
+                for(;i<response.body().getTracks().getTrack().size();i++){
                     arrayList.add(new Model(response.body().getTracks().getTrack().get(i).getName(),
                             response.body().getTracks().getTrack().get(i).getArtist().getName(),
                             response.body().getTracks().getTrack().get(i).getAttr().getRank()));
                     Log.d("gelenler2",response.body().getTracks().getTrack().get(2).getArtist().getName());
                 }
                 adaptor adaptor= new adaptor(MainActivity.this,arrayList);
-                Log.d("gelenler",arrayList.get(1).getIsim());
+                Log.d("gelenler3", String.valueOf(arrayList.size()));
                 recyclerView.setAdapter(adaptor);
                 recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
             }
@@ -49,7 +63,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<Gelenler> call, Throwable t) {
                 Log.d("gelenler",t.getMessage());
-                textView.setText(t.getMessage());
+            }
+        });
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (recyclerView.canScrollVertically(2) && newState==RecyclerView.SCROLL_STATE_IDLE) {
+                    api();
+                }
             }
         });
     }
